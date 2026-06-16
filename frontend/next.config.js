a13e8ee3d638e2f9ffd927f6a82fddb809fb1526
gergel <54780904+gergel@users.noreply.github.com>
2026-06-16 19:16:55 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,13 +4,5 @@ const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"}/api/:path*`,
-      },
-    ];
-  },
 };
 module.exports = nextConfig;
